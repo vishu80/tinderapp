063fea004daa0a5751019f280c6434e2bd29a940
vishal <vishalsharma9522@gmail.com>
2024-10-17 15:00:31 +0530

    multile handler callbackfunction inside express is called handler

--- src/app.js
+++ src/app.js
@@ -1,30 +1,47 @@
-const express=require('express');
+const express = require("express");
 const app = express();
 
-app.get('/user/:name', (req, res) => {
-  res.send({
-    name:req.params.name,
-    id:req.query.id,
-    value:req.query.value
-  });
-});
-app.get(/a/,(req, res) => {
-
-  res.send('test the regex api request');
-})
-app.post('/user',(req,res)=>{
-  res.send('test the  api request post api');
-
-})
-app.delete('/user',(req,res)=>{
-  res.send('First api request deleted');
-
-})
-app.put('/',(req,res)=>{
-  res.send('Welcome to my API put');
- });
+// app.get("/user/:name", (req, res) => {
+//   res.send({
+//     name: req.params.name,
+//     id: req.query.id,
+//     value: req.query.value,
+//   });
+// });
+// // app.get(/a/, (req, res) => {
+// //   res.send("test the regex api request");
+// // });
+// app.post("/user", (req, res) => {
+//   res.send("test the  api request post api");
+// });
+// app.delete("/user", (req, res) => {
+//   res.send("First api request deleted");
+// });
 
 
+// app.put("/", (req, res) => {
+//   res.send("Welcome to my API put");
+// });
 
+//Routing and multple middleware for expres
 
-app.listen(7777)
+const cb1=(req,res,next)=>{
+  console.log('cb1 function was called');
+  next();
+
+}
+
+const cb2=(req,res,next)=>{
+  console.log('cb2 functionwas called');
+  next();
+}
+
+const cb3=(req,res,next)=>{
+console.log('cb3 functionwas called');
+res.send('finally cb3 was called')
+}
+app.post(
+  "/admin",[cb1, cb2, cb3]
+
+);
+app.listen(7777);
